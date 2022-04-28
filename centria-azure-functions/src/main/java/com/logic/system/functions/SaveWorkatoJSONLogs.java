1ce52d0b2ce9bbc22ba1f6a6e3646260ae4913a1
alejandroheba-centria <alejandro.hernandez@centriahealthcare.com>
2022-04-28 15:44:50 -0400

    separate functions

---
+++ centria-azure-functions/src/main/java/com/logic/system/functions/SaveWorkatoJSONLogs.java
@@ -0,0 +1,49 @@
+package com.logic.system.functions;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.logic.system.functions.cosmos.model.LogItem;
+import com.microsoft.azure.functions.*;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.CosmosDBOutput;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
+import java.util.logging.Level;
+
+public class SaveWorkatoJSONLogs {
+    @FunctionName("Save-Workato-JSON-Logs")
+    public HttpResponseMessage run(
+            @HttpTrigger(    name      = "req",
+                    methods   = {HttpMethod.POST},
+                    authLevel = AuthorizationLevel.FUNCTION,
+                    route     = "Save-Workato-JSON-Logs/saveLog")
+                    HttpRequestMessage<Optional<String>> request,
+            @CosmosDBOutput( name                    = "databaseForErrorLogs",
+                    databaseName            = "%COSMOS_ERROR_LOG_DB_NAME%",
+                    collectionName          = "%COSMOS_ERROR_LOG_COLLECTION_NAME%",
+                    connectionStringSetting          = "COSMOS_ERROR_LOG_CONNECTION_STRING_SETTING")
+                    OutputBinding<LogItem> outputItem,
+            final ExecutionContext context) {
+        context.getLogger().info("Save-Workato-JSON-Logs/saveLog trigger processed a request.");
+        Map<String, String> headers = request.getHeaders();
+        final String jsonString = request.getBody().orElse(null);
+        if (jsonString != null && headers.get("content-type").equals("application/json")) {
+            try {
+                GsonBuilder builder = new GsonBuilder();
+                builder.setPrettyPrinting();
+                Gson gson = builder.create();
+                LogItem item = gson.fromJson(jsonString, LogItem.class);
+                item.setId(new StringBuffer().append(Math.abs(new Random().nextInt())).toString());
+                outputItem.setValue(item);
+                return request.createResponseBuilder(HttpStatus.OK).body("JSON log object saved successfully").build();
+            } catch (Exception e) {
+                context.getLogger().log(Level.SEVERE,"Save-Workato-JSON-Logs/saveLog had a problem getting the JSON Object. Message = "+e.getMessage(), e);
+            }
+        }
+        return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass the json object in the body").build();
+    }
+}
--- centria-azure-functions/src/main/java/com/logic/system/functions/SsrsNonBillableReportFTPTransferFunction.java
+++ centria-azure-functions/src/main/java/com/logic/system/functions/SsrsNonBillableReportFTPTransferFunction.java
@@ -22,7 +22,7 @@ import java.util.Properties;
 import java.util.Random;
 import java.util.logging.Level;
 
-public class CentriaFunctions {
+public class SsrsNonBillableReportFTPTransferFunction {
     private static final String INBOUND_FOLDER_SSRS_REPORT;
     private static final String ARCHIVED_FOLDER_SSRS_REPORT;
     private static final String FILE_SHARE_SSRS_REPORTS;
@@ -51,39 +51,6 @@ public class CentriaFunctions {
         SFTP_PASSWORD = System.getenv("SFTP_PASSWORD");
         SFTP_DESTINY_FOLDER = System.getenv("SFTP_DESTINY_FOLDER");
     }
-
-    @FunctionName("Save-Workato-JSON-Logs")
-    public HttpResponseMessage run(
-            @HttpTrigger(    name      = "req",
-                             methods   = {HttpMethod.POST},
-                             authLevel = AuthorizationLevel.FUNCTION,
-                             route     = "Save-Workato-JSON-Logs/saveLog")
-            HttpRequestMessage<Optional<String>> request,
-            @CosmosDBOutput( name                    = "databaseForErrorLogs",
-                             databaseName            = "%COSMOS_ERROR_LOG_DB_NAME%",
-                             collectionName          = "%COSMOS_ERROR_LOG_COLLECTION_NAME%",
-                    connectionStringSetting          = "COSMOS_ERROR_LOG_CONNECTION_STRING_SETTING")
-            OutputBinding<LogItem> outputItem,
-            final ExecutionContext context) {
-        context.getLogger().info("Save-Workato-JSON-Logs/saveLog trigger processed a request.");
-        Map<String, String> headers = request.getHeaders();
-        final String jsonString = request.getBody().orElse(null);
-        if (jsonString != null && headers.get("content-type").equals("application/json")) {
-            try {
-                GsonBuilder builder = new GsonBuilder();
-                builder.setPrettyPrinting();
-                Gson gson = builder.create();
-                LogItem item = gson.fromJson(jsonString, LogItem.class);
-                item.setId(new StringBuffer().append(Math.abs(new Random().nextInt())).toString());
-                outputItem.setValue(item);
-                return request.createResponseBuilder(HttpStatus.OK).body("JSON log object saved successfully").build();
-            } catch (Exception e) {
-                context.getLogger().log(Level.SEVERE,"Save-Workato-JSON-Logs/saveLog had a problem getting the JSON Object. Message = "+e.getMessage(), e);
-            }
-        }
-        return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass the json object in the body").build();
-    }
-
 
     @FunctionName("SsrsNonBillableReportFTPTransferFunction-Java")
     public void run(                                 /*Every 5 minutes from 7AM to 9 PM*/
