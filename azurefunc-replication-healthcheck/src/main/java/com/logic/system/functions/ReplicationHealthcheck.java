727fa0868e28e61aafe102ce642fe51932c28a32
alejandroheba-centria <alejandro.hernandez@centriahealthcare.com>
2022-05-20 09:50:33 -0400

    adding logs

--- azurefunc-replication-healthcheck/src/main/java/com/logic/system/functions/ReplicationHealthcheck.java
+++ azurefunc-replication-healthcheck/src/main/java/com/logic/system/functions/ReplicationHealthcheck.java
@@ -2,6 +2,8 @@ package com.logic.system.functions;
 
 import java.sql.*;
 import java.time.*;
+import java.util.logging.Level;
+
 import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.*;
 
@@ -26,17 +28,23 @@ public class ReplicationHealthcheck {
 
         Connection conn = null;
         try {
+            context.getLogger().info("Getting connection with "+REPLICATION_DB_CONNECTION_STRING);
             conn = DriverManager.getConnection(REPLICATION_DB_CONNECTION_STRING);
+            context.getLogger().info("Creating Statement.....");
             Statement statement = conn.createStatement();
+            context.getLogger().info("Executing Query.....");
             ResultSet rs = statement.executeQuery("select top 10 * from system_code ");
             while (rs.next()) {
                 String codeDesc = rs.getString("code_desc_en");
                 context.getLogger().info("Code Description: " + codeDesc);
             }
 
-        } catch (SQLException throwables) {
-                throwables.printStackTrace();
+        } catch (SQLException e) {
+            context.getLogger().info("Error while connection to DB: " + e.getMessage());
+            context.getLogger().log(Level.SEVERE, "SQL Problem ...", e);
         }
+
+        context.getLogger().info("Java Timer trigger function finieshed at: " + LocalDateTime.now());
 
 
     }
